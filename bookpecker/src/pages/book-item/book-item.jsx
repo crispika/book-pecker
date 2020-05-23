bde296a490bd3341c47f59e7b67e618aa2fa333c
lijundong <leeonfield@foxmail.com>
2020-05-23 12:02:56 +0800

    :lipstick: update list and item style

--- bookpecker/src/pages/book-item/book-item.jsx
+++ bookpecker/src/pages/book-item/book-item.jsx
@@ -1,96 +1,89 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types';
-import { Card, Typography } from 'antd';
-import { StarOutlined, ShareAltOutlined, ShoppingCartOutlined } from '@ant-design/icons';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Card, Typography } from "antd";
+import {
+  StarOutlined,
+  ShareAltOutlined,
+  ShoppingCartOutlined,
+} from "@ant-design/icons";
 import { Link } from "react-router-dom";
 import "react-router-dom";
+import { connect } from "react-redux";
+import { deleteBook } from "../../redux/actions.js";
+import { addToCart } from "../../utils/handleEvent";
+import { formatMoney } from "../../utils/money-calculator";
 
-import "./book-item.less"
-import { connect } from 'react-redux';
-import { deleteBook } from "../../redux/actions.js"
-import {addToCart} from "../../utils/handleEvent"
-import {formatMoney} from "../../utils/money-calculator"
-
-
+import "./book-item.less";
 
 const { Meta } = Card;
-const {Text, Paragraph} = Typography;
+const { Text } = Typography;
 
 class BookItem extends Component {
+  // 添加书进购物车 - 从util中复用
+  // addToCart = (id, bookname) => {
 
-    // 添加书进购物车 - 从util中复用
-    // addToCart = (id, bookname) => {
+  //     // 存入localstorage
+  //     let trolleyDB = getTrolley() || {};
+  //     trolleyDB[id] = bookname;
+  //     updateTrolley(trolleyDB);
 
-    //     // 存入localstorage
-    //     let trolleyDB = getTrolley() || {};
-    //     trolleyDB[id] = bookname;
-    //     updateTrolley(trolleyDB);
+  //     // 弹出确认加入购物车的对话框
+  //     notifySuccess('加入购物车成功！',`${bookname}已经加入购物车。`)
 
-    //     // 弹出确认加入购物车的对话框
-    //     notifySuccess('加入购物车成功！',`${bookname}已经加入购物车。`)
+  //     //  从书的列表中移除
+  //     // this.props.deleteBook(id);
 
-    //     //  从书的列表中移除
-    //     // this.props.deleteBook(id);
+  // }
 
-    // }
-
-    render() {
-        const { bookname, short_intro, cover, id, price } = this.props.book_info;
-        return (
-            <Card
-                hoverable
-                bordered={false}
-                style={{ width: 300, margin: "0 auto"}}
-                // bodyStyle={{width: 300, margin: "0 auto"}}
-                cover={
-                    <Link to={`/description/${id}`} >
-                    <div style={{
-                        width: '100%',
-                        height: '425px',
-                        background: `url(${cover}) no-repeat center`,
-                        backgroundSize: 'cover',
-                        // boxSizing:"border-box",
-                        // border:"1px solid"
-                    }}></div>
-                        {/* <img
-                            style={{width:"100%", height: '210px'}}
-                            alt="book-cover"
-                            // src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-                            // src="http://huaxia.com/zhwh/yd/images/2018/09/18/2097945.png"
-                            src={cover}
-                        /> */}
-                    </Link>
-                }
-                actions={[
-                    <StarOutlined key="wish" />,
-                    <ShoppingCartOutlined key="trolley" onClick={addToCart.bind(this, id, bookname)} />,
-                    <ShareAltOutlined key="share" />,
-                ]}
-            >
-                <Link to={`/description/${id}`} >
-                <Meta
-                    title={bookname}
-                    description={
-                        <div>
-                            <p style={{marginBottom: 5, marginLeft:-2}}><Text type="warning">{formatMoney(price)}</Text></p>
-                            <Paragraph type="secondary" style={{marginBottom: 5}}>{short_intro}</Paragraph>
-                        </div>
-                        
-                    }
-                />
-                </Link>
-            </Card>
-        )
-    }
+  render() {
+    const { bookname, short_intro, cover, id, price } = this.props.book_info;
+    return (
+      <Card
+        hoverable
+        bordered={false}
+        style={{ width: 300, margin: "0 auto" }}
+        cover={
+          <Link to={`/description/${id}`}>
+            <div
+              style={{
+                width: "100%",
+                height: "425px",
+                background: `url(${cover}) no-repeat center`,
+                backgroundSize: "cover",
+              }}
+            ></div>
+          </Link>
+        }
+        actions={[
+          <StarOutlined key="wish" />,
+          <ShoppingCartOutlined
+            key="trolley"
+            onClick={addToCart.bind(this, id, bookname)}
+          />,
+          <ShareAltOutlined key="share" />,
+        ]}
+      >
+        <Link to={`/description/${id}`}>
+          <Meta
+            title={bookname}
+            description={
+              <div>
+                <p style={{ marginBottom: 5, marginLeft: -2 }}>
+                  <Text type="warning">{formatMoney(price)}</Text>
+                </p>
+                <p className="book-item-desc">{short_intro}</p>
+              </div>
+            }
+          />
+        </Link>
+      </Card>
+    );
+  }
 }
-
 
 BookItem.propTypes = {
-    book_info: PropTypes.object.isRequired,
-    deleteBook: PropTypes.func.isRequired,
-}
+  book_info: PropTypes.object.isRequired,
+  deleteBook: PropTypes.func.isRequired,
+};
 
-export default connect(
-    null,
-    { deleteBook }
-)(BookItem)
+export default connect(null, { deleteBook })(BookItem);
--- bookpecker/src/pages/book-list/book-list.jsx
+++ bookpecker/src/pages/book-list/book-list.jsx
@@ -1,78 +1,76 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 import BookItem from "../book-item/book-item";
-import { Row, Col, Pagination } from 'antd';
+import { Row, Col, Pagination } from "antd";
 import { pages_bg_color } from "../../assets/color";
-import { connect } from 'react-redux';
-import { getBookList, clearStateToObj } from "../../redux/actions.js"
-
-
+import { connect } from "react-redux";
+import { getBookList, clearStateToObj } from "../../redux/actions.js";
+import './book-list.less'
 
 class BookList extends Component {
-    constructor(props) {
-        super(props)
-        // console.log(this.props)
-    }
+  constructor(props) {
+    super(props);
+    // console.log(this.props)
+  }
 
-    componentDidMount() {
-        // debugger
-        this.props.getBookList();
-    }
+  componentDidMount() {
+    // debugger
+    this.props.getBookList();
+  }
 
-    // // 将已经加入购物车的书从列表页中移除
-    // pickBook = (id) => {
-    //     const picked_list = this.state.book_list.filter(book => book.id !== id)
-    //     this.setState({book_list:picked_list})
-    // }
+  // // 将已经加入购物车的书从列表页中移除
+  // pickBook = (id) => {
+  //     const picked_list = this.state.book_list.filter(book => book.id !== id)
+  //     this.setState({book_list:picked_list})
+  // }
 
-    componentWillUnmount() {
-        this.props.clearStateToObj();
-    }
+  componentWillUnmount() {
+    this.props.clearStateToObj();
+  }
 
-    render() {
-        // debugger
-        const { book_list } = this.props;
-        return (
-            <div className="book-list-wrapper"
-                style={{
-                    backgroundColor: pages_bg_color,
-                    margin: "auto 270px",
-                    marginTop: "20px",
-                    marginBottom: "20px",
-                }}
-            >
-                {/* 栅格布局套card样式的book-item */}
-                {Object.keys(book_list).length > 0 &&
-                    (<Row justify="space-around" align="top" gutter={[32, { xs: 8, sm: 16, md: 24, lg: 32 }]}>
-                        {book_list.map(
-                            (book_info, index) =>
-                                <Col span={6} key={index}>
-                                    {/* <BookItem  book_info={book_info} pickBook={this.pickBook}/> */}
-                                    <BookItem book_info={book_info} />
-                                </Col>
-                        )}
-                    </Row>)}
+  render() {
+    // debugger
+    const { book_list } = this.props;
+    return (
+      <div
+        className="book-list-wrapper"
+      >
+        {/* 栅格布局套card样式的book-item */}
+        {Object.keys(book_list).length > 0 && (
+          <Row
+            justify="space-between"
+            align="top"
+            gutter={[32, { xs: 8, sm: 16, md: 24, lg: 32 }]}
+          >
+            {book_list.map((book_info, index) => (
+              <Col span={6} key={index}>
+                {/* <BookItem  book_info={book_info} pickBook={this.pickBook}/> */}
+                <BookItem book_info={book_info} />
+              </Col>
+            ))}
+          </Row>
+        )}
 
-                {/* 分页符 */}
-                <Pagination
-                    style={{ width: "200px", margin: "0 auto" }}
-                    // simple 
-                    defaultCurrent={1}
-                    total={27}
-                    pageSize={9}
-                />
-            </div>
-        )
-    }
+        {/* 分页符 */}
+        <Pagination
+          style={{ width: "200px", margin: "0 auto" }}
+          // simple
+          defaultCurrent={1}
+          total={27}
+          pageSize={9}
+        />
+      </div>
+    );
+  }
 }
 
 BookList.propTypes = {
-    getBookList: PropTypes.func.isRequired,
-    clearStateToObj: PropTypes.func.isRequired,
-    book_list: PropTypes.array.isRequired,
-}
+  getBookList: PropTypes.func.isRequired,
+  clearStateToObj: PropTypes.func.isRequired,
+  book_list: PropTypes.array.isRequired,
+};
 
-export default connect(
-    state => ({ book_list: state.book_list }),
-    { getBookList, clearStateToObj }
-)(BookList)
+export default connect((state) => ({ book_list: state.book_list }), {
+  getBookList,
+  clearStateToObj,
+})(BookList);
